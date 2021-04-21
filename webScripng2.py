9160cc18121c736d5a1ab58e4ca7f29c99814eef
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-04-20 22:09:15 -0300

    criado o repositorio

---
+++ webScripng2.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup as bp
+import requests
+import csv
+
+
+csv_file = open('fornecedores.csv', 'w', newline='')
+csv_write = csv.writer(csv_file)
+csv_write.writerow(['id', 'empresa', 'telefone', 'site', 'email'])
+
+url = 'http://www.abimad.com.br/associados/pesquisar#menu-top'
+
+r = requests.get(url)
+
+sup = bp(r.content, 'html.parser')
+
+todos_elementos = sup.find_all('figure')
+
+id = 0
+
+for elemento in todos_elementos:
+    empresa = elemento.h5.text
+    telefone = elemento.find(
+        'i', class_='fa-phone').next_element.next_element.text
+    site = elemento.find('i', class_='fa-link').next_element.next_element.text
+    email = elemento.find('i', class_='fa-at').next_element.next_element.text
+
+    print('empresa:', empresa)
+    print('telefone:', telefone)
+    print('site:', site)
+    print('email:', email)
+    print('-'*40)
+    csv_write.writerow([id, empresa, telefone, site, email])
+    id += 1
+
+csv_file.close()
